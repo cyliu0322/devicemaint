f79743556e77e6984a7fa332e2b3ffe3514557c4
cyliu0322 <cyliu0322@163.com>
2019-11-15 09:57:48 +0800

    Signed-off-by: cyliu0322 <cyliu0322@163.com>

--- src/main/java/com/maint/common/config/ShiroConfig.java
+++ src/main/java/com/maint/common/config/ShiroConfig.java
@@ -64,14 +64,14 @@ public class ShiroConfig {
 	@Resource
 	private ShiroActionProperties shiroActionProperties;
 	
-//	@Value("${spring.redis.host}")
-//	private String redisHost;
-//	
-//	@Value("${spring.redis.port}")
-//	private Integer redisPort;
+	@Value("${spring.redis.host}")
+	private String redisHost;
 	
-	@Value("${ehcache.config-location}")
-	private String ehcachePath;
+	@Value("${spring.redis.port}")
+	private Integer redisPort;
+	
+//	@Value("${ehcache.config-location}")
+//	private String ehcachePath;
 	
 	@Bean
 	public RestShiroFilterFactoryBean restShiroFilterFactoryBean(SecurityManager securityManager) {
@@ -127,8 +127,8 @@ public class ShiroConfig {
 	public UserNameRealm userNameRealm() {
 		UserNameRealm userNameRealm = new UserNameRealm();
 		userNameRealm.setCredentialsMatcher(hashedCredentialsMatcher());
-//		userNameRealm.setCacheManager(redisCacheManager());
-		userNameRealm.setCacheManager(ehCacheManager());
+		userNameRealm.setCacheManager(redisCacheManager());
+//		userNameRealm.setCacheManager(ehCacheManager());
 		return userNameRealm;
 	}
 	
@@ -145,102 +145,102 @@ public class ShiroConfig {
 		return new ShiroDialect();
 	}
 	
-//	@Bean
-//	public RedisCacheManager redisCacheManager() {
-//		RedisCacheManager redisCacheManager = new RedisCacheManager();
-//		redisCacheManager.setRedisManager(redisManager());
-//		redisCacheManager.setExpire(shiroActionProperties.getPermsCacheTimeout() == null ? 3600
-//				: shiroActionProperties.getPermsCacheTimeout());
-//		redisCacheManager.setPrincipalIdFieldName("userId");
-//		return redisCacheManager;
-//	}
-//	
-//	@Bean
-//	public RedisManager redisManager() {
-//		RedisManager redisManager = new RedisManager();
-//		redisManager.setHost(redisHost + ":" + redisPort);
-//		return redisManager;
-//	}
-//	
-//	@Bean
-//	public RedisSessionDAO redisSessionDAO() {
-//		RedisSessionDAO redisSessionDAO = new RedisSessionDAO();
-//		redisSessionDAO.setExpire(
-//				shiroActionProperties.getSessionTimeout() == null ? 1800 : shiroActionProperties.getSessionTimeout());
-//		redisSessionDAO.setRedisManager(redisManager());
-//		redisSessionDAO.setSessionInMemoryEnabled(false);
-//		return redisSessionDAO;
-//	}
-//	
-//	@Bean
-//	public DefaultWebSessionManager sessionManager() {
-//		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
-//		sessionManager.setSessionDAO(redisSessionDAO());
-//		sessionManager.setSessionIdUrlRewritingEnabled(false);
-//		return sessionManager;
-//	}
-	
-	/**
-	 * ehcache缓存管理器；shiro整合ehcache： 通过安全管理器：securityManager 单例的cache防止热部署重启失败
-	 * @return EhCacheManager
-	 */
 	@Bean
-	public EhCacheManager ehCacheManager() {
-		EhCacheManager ehcache = new EhCacheManager();
-		CacheManager cacheManager = CacheManager.getCacheManager("shiro");
-		if (cacheManager == null) {
-			try {
-				cacheManager = CacheManager.create(ResourceUtils.getInputStreamForPath(ehcachePath));
-			} catch (CacheException | IOException e) {
-				e.printStackTrace();
-			}
-		}
-		ehcache.setCacheManager(cacheManager);
-		return ehcache;
+	public RedisCacheManager redisCacheManager() {
+		RedisCacheManager redisCacheManager = new RedisCacheManager();
+		redisCacheManager.setRedisManager(redisManager());
+		redisCacheManager.setExpire(shiroActionProperties.getPermsCacheTimeout() == null ? 3600
+				: shiroActionProperties.getPermsCacheTimeout());
+		redisCacheManager.setPrincipalIdFieldName("userId");
+		return redisCacheManager;
 	}
 	
-	/**
-	 * EnterpriseCacheSessionDAO shiro sessionDao层的实现；
-	 * 提供了缓存功能的会话维护，默认情况下使用MapCache实现，内部使用ConcurrentHashMap保存缓存的会话。
-	 */
 	@Bean
-	public EnterpriseCacheSessionDAO enterCacheSessionDAO() {
-		EnterpriseCacheSessionDAO enterCacheSessionDAO = new EnterpriseCacheSessionDAO();
-		// 添加ehcache活跃缓存名称（必须和ehcache缓存名称一致）
-		enterCacheSessionDAO.setActiveSessionsCacheName("shiro-activeSessionCache");
-		return enterCacheSessionDAO;
+	public RedisManager redisManager() {
+		RedisManager redisManager = new RedisManager();
+		redisManager.setHost(redisHost + ":" + redisPort);
+		return redisManager;
 	}
 	
-	/**
-	 *
-	 * @描述：sessionManager添加session缓存操作DAO
-	 * @创建人：wyait
-	 * @创建时间：2018年4月24日 下午8:13:52
-	 * @return
-	 */
+	@Bean
+	public RedisSessionDAO redisSessionDAO() {
+		RedisSessionDAO redisSessionDAO = new RedisSessionDAO();
+		redisSessionDAO.setExpire(
+				shiroActionProperties.getSessionTimeout() == null ? 1800 : shiroActionProperties.getSessionTimeout());
+		redisSessionDAO.setRedisManager(redisManager());
+		redisSessionDAO.setSessionInMemoryEnabled(false);
+		return redisSessionDAO;
+	}
+	
 	@Bean
 	public DefaultWebSessionManager sessionManager() {
 		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
-		sessionManager.setSessionDAO(enterCacheSessionDAO());
-		sessionManager.setSessionIdCookie(sessionIdCookie());
+		sessionManager.setSessionDAO(redisSessionDAO());
+		sessionManager.setSessionIdUrlRewritingEnabled(false);
 		return sessionManager;
 	}
 	
-	/**
-	 *
-	 * @描述：自定义cookie中session名称等配置
-	 * @创建人：wyait
-	 * @创建时间：2018年5月8日 下午1:26:23
-	 * @return
-	 */
-	@Bean
-	public SimpleCookie sessionIdCookie() {
-		// DefaultSecurityManager
-		SimpleCookie simpleCookie = new SimpleCookie();
-		// 如果在Cookie中设置了"HttpOnly"属性，那么通过程序(JS脚本、Applet等)将无法读取到Cookie信息，这样能有效的防止XSS攻击。
-		simpleCookie.setHttpOnly(true);
-		simpleCookie.setName("SHRIOSESSIONID");
-		simpleCookie.setMaxAge(86400);// 30分钟
-		return simpleCookie;
-	}
+//	/**
+//	 * ehcache缓存管理器；shiro整合ehcache： 通过安全管理器：securityManager 单例的cache防止热部署重启失败
+//	 * @return EhCacheManager
+//	 */
+//	@Bean
+//	public EhCacheManager ehCacheManager() {
+//		EhCacheManager ehcache = new EhCacheManager();
+//		CacheManager cacheManager = CacheManager.getCacheManager("shiro");
+//		if (cacheManager == null) {
+//			try {
+//				cacheManager = CacheManager.create(ResourceUtils.getInputStreamForPath(ehcachePath));
+//			} catch (CacheException | IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		ehcache.setCacheManager(cacheManager);
+//		return ehcache;
+//	}
+//	
+//	/**
+//	 * EnterpriseCacheSessionDAO shiro sessionDao层的实现；
+//	 * 提供了缓存功能的会话维护，默认情况下使用MapCache实现，内部使用ConcurrentHashMap保存缓存的会话。
+//	 */
+//	@Bean
+//	public EnterpriseCacheSessionDAO enterCacheSessionDAO() {
+//		EnterpriseCacheSessionDAO enterCacheSessionDAO = new EnterpriseCacheSessionDAO();
+//		// 添加ehcache活跃缓存名称（必须和ehcache缓存名称一致）
+//		enterCacheSessionDAO.setActiveSessionsCacheName("shiro-activeSessionCache");
+//		return enterCacheSessionDAO;
+//	}
+//	
+//	/**
+//	 *
+//	 * @描述：sessionManager添加session缓存操作DAO
+//	 * @创建人：wyait
+//	 * @创建时间：2018年4月24日 下午8:13:52
+//	 * @return
+//	 */
+//	@Bean
+//	public DefaultWebSessionManager sessionManager() {
+//		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+//		sessionManager.setSessionDAO(enterCacheSessionDAO());
+//		sessionManager.setSessionIdCookie(sessionIdCookie());
+//		return sessionManager;
+//	}
+//	
+//	/**
+//	 *
+//	 * @描述：自定义cookie中session名称等配置
+//	 * @创建人：wyait
+//	 * @创建时间：2018年5月8日 下午1:26:23
+//	 * @return
+//	 */
+//	@Bean
+//	public SimpleCookie sessionIdCookie() {
+//		// DefaultSecurityManager
+//		SimpleCookie simpleCookie = new SimpleCookie();
+//		// 如果在Cookie中设置了"HttpOnly"属性，那么通过程序(JS脚本、Applet等)将无法读取到Cookie信息，这样能有效的防止XSS攻击。
+//		simpleCookie.setHttpOnly(true);
+//		simpleCookie.setName("SHRIOSESSIONID");
+//		simpleCookie.setMaxAge(86400);// 30分钟
+//		return simpleCookie;
+//	}
 }
