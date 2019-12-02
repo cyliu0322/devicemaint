97f9b91dca07617e70067bf2c03b952fca04dfbc
cyliu0322 <cyliu0322@163.com>
2019-12-02 09:31:57 +0800

    Signed-off-by: cyliu0322 <cyliu0322@163.com>

--- src/main/java/com/maint/system/controller/DeviceController.java
+++ src/main/java/com/maint/system/controller/DeviceController.java
@@ -4,9 +4,11 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -46,5 +48,21 @@ public class DeviceController {
 	@ResponseBody
 	public ResultBean add(Device device) {
 		return ResultBean.success(deviceService.add(device));
+	}
+	
+	@OperationLog("编辑设备")
+	@PutMapping
+	@ResponseBody
+	public ResultBean update(Device device) {
+		deviceService.update(device);
+		return ResultBean.success();
+	}
+	
+	@OperationLog("删除设备")
+	@DeleteMapping("/{deviceId}")
+	@ResponseBody
+	public ResultBean delete(@PathVariable("deviceId") String deviceId) {
+		deviceService.delete(deviceId);
+		return ResultBean.success();
 	}
 }
